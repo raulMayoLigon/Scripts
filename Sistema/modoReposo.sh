75137ce60940dc3dc049a6a0d9f4af877e7096f6
Antonio <antonioangosoo@gmail.com>
2025-04-20 00:33:42 +0200

    Actualización: 2025-04-20 00:33:42

--- Sistema/modoReposo.sh
+++ Sistema/modoReposo.sh
@@ -1,2 +1,29 @@
 #!/bin/bash
-pmset sleepnow
+
+echo "Aplicaciones activas:"
+osascript -e 'tell application "System Events" to get name of (processes where background only is false)'
+
+echo
+echo "¿Qué deseas hacer?"
+echo "1) Suspender (reposo)"
+echo "2) Apagar"
+echo "3) Cancelar"
+
+read -p "Introduce una opción (1-3): " opcion
+
+case $opcion in
+  1)
+    echo "Poniendo el sistema en reposo..."
+    pmset sleepnow
+    ;;
+  2)
+    echo "Apagando el sistema..."
+    sudo shutdown -h now
+    ;;
+  3)
+    echo "Operación cancelada."
+    ;;
+  *)
+    echo "Opción no válida."
+    ;;
+esac
